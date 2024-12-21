af3a53388d5bdb65fc9845854ad244ee851d918a
Jiazheng Dou <djz@DJZs-mbp.local>
2024-12-21 15:40:00 +0800

    new

--- rsync_to_mac.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-
-des="djz@192.168.4.81:~/Downloads/plots_paper/"
-src="/home/doujzh/Documents/djzfiles/plots_paper/"
-
-rsync -azvrP --progress --exclude *.fits --copy-links --rsh="/usr/bin/sshpass -p 690109 ssh -o StrictHostKeyChecking=no -l djz" $src  $des
